f00b968399408da1d34242dffe7ebaa4a3c1679d
tencentcloudapi <tencentcloudapi@tenent.com>
2020-10-13 09:03:56 +0800

    release 3.0.264

--- src/TencentCloud/Gse/V20191112/Models/CreateAssetResponse.php
+++ src/TencentCloud/Gse/V20191112/Models/CreateAssetResponse.php
@@ -22,6 +22,8 @@ use TencentCloud\Common\AbstractModel;
  *
  * @method string getAssetId() 获取生成包ID
  * @method void setAssetId(string $AssetId) 设置生成包ID
+ * @method string getAssetArn() 获取生成包的全局唯一资源标识符
+ * @method void setAssetArn(string $AssetArn) 设置生成包的全局唯一资源标识符
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
@@ -33,12 +35,18 @@ class CreateAssetResponse extends AbstractModel
     public $AssetId;
 
     /**
+     * @var string 生成包的全局唯一资源标识符
+     */
+    public $AssetArn;
+
+    /**
      * @var string 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
      * @param string $AssetId 生成包ID
+     * @param string $AssetArn 生成包的全局唯一资源标识符
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -56,6 +64,10 @@ class CreateAssetResponse extends AbstractModel
         }
         if (array_key_exists("AssetId",$param) and $param["AssetId"] !== null) {
             $this->AssetId = $param["AssetId"];
+        }
+
+        if (array_key_exists("AssetArn",$param) and $param["AssetArn"] !== null) {
+            $this->AssetArn = $param["AssetArn"];
         }
 
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
